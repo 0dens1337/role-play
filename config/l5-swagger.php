2e00abc95efec0b4b6f93948966b408682d2cdfe
0dens1337 <m.mahov@framework.team>
2025-08-20 16:30:12 +0400

    fix caddy | swagger link

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -12,7 +12,7 @@ return [
                 /*
                  * Route for accessing api documentation interface
                  */
-                'api' => 'api/role-play/v1/documentation',
+                'api' => 'api/documentation',
             ],
             'paths' => [
                 /*
